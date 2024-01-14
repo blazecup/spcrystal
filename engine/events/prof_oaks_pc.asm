aaaaaa123456789/pokecrystal16

Update the Pokédex to use 16-bit IDs

engine/events/prof_oaks_pc.asm
@@ -33,21 +33,25 @@ ProfOaksPCRating:
 
 Rate:
 ; calculate Seen/Owned
-	ld hl, wPokedexSeen
-	ld b, wEndPokedexSeen - wPokedexSeen
-	call CountSetBits
-	ld [wd002], a
 	ld hl, wPokedexCaught
-	ld b, wEndPokedexCaught - wPokedexCaught
-	call CountSetBits
-	ld [wd003], a
+	ld bc, wEndPokedexCaught - wPokedexCaught
+	call CountSetBits16
+	push bc
+	ld hl, wPokedexSeen
+	ld bc, wEndPokedexSeen - wPokedexSeen
+	call CountSetBits16
 
 ; print appropriate rating
-	call .UpdateRatingBuffers
+	ld hl, wStringBuffer3
+	call .UpdateRatingBuffer
+	pop bc
+	push bc
+	ld hl, wStringBuffer4
+	call .UpdateRatingBuffer
 	ld hl, OakPCText3
 	call PrintText
 	call JoyWaitAorB
-	ld a, [wd003]
+	pop bc
 	ld hl, OakRatings
 	call FindOakRating
 	push de
@@ -55,38 +59,41 @@ Rate:
 	pop de
 	ret
 
-.UpdateRatingBuffers:
-	ld hl, wStringBuffer3
-	ld de, wd002
-	call .UpdateRatingBuffer
-	ld hl, wStringBuffer4
-	ld de, wd003
-	call .UpdateRatingBuffer
-	ret
-
 .UpdateRatingBuffer:
+	ld a, b
+	ld b, c
+	ld c, a
+	push bc
 	push hl
 	ld a, "@"
 	ld bc, ITEM_NAME_LENGTH
 	call ByteFill
+	ld hl, sp + 2
+	ld d, h
+	ld e, l
 	pop hl
-	lb bc, PRINTNUM_RIGHTALIGN | 1, 3
+	lb bc, PRINTNUM_RIGHTALIGN | 2, 4
 	call PrintNum
+	pop bc
 	ret
 
 FindOakRating:
 ; return sound effect in de
 ; return text pointer in hl
-	nop
-	ld c, a
-.loop
 	ld a, [hli]
+	ld d, a
+	ld a, [hli]
+	cp b
+	jr c, .next
+	jr nz, .match
+	ld a, d
 	cp c
 	jr nc, .match
-rept 4
-	inc hl
-endr
-	jr .loop
+.next
+	rept 4
+		inc hl
+	endr
+	jr FindOakRating
 
 .match
 	ld a, [hli]

blazecup/spcrystal

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/events/prof_oaks_pc.asm
@@ -33,21 +33,25 @@ ProfOaksPCRating:
 
 Rate:
 ; calculate Seen/Owned
-	ld hl, wPokedexSeen
-	ld b, wEndPokedexSeen - wPokedexSeen
-	call CountSetBits
-	ld [wTempPokedexSeenCount], a
 	ld hl, wPokedexCaught
-	ld b, wEndPokedexCaught - wPokedexCaught
-	call CountSetBits
-	ld [wTempPokedexCaughtCount], a
+	ld bc, wEndPokedexCaught - wPokedexCaught
+	call CountSetBits16
+	push bc
+	ld hl, wPokedexSeen
+	ld bc, wEndPokedexSeen - wPokedexSeen
+	call CountSetBits16
 
 ; print appropriate rating
-	call .UpdateRatingBuffers
+	ld hl, wStringBuffer3
+	call .UpdateRatingBuffer
+	pop bc
+	push bc
+	ld hl, wStringBuffer4
+	call .UpdateRatingBuffer
 	ld hl, OakPCText3
 	call PrintText
 	call JoyWaitAorB
-	ld a, [wTempPokedexCaughtCount]
+	pop bc
 	ld hl, OakRatings
 	call FindOakRating
 	push de
@@ -55,38 +59,41 @@ Rate:
 	pop de
 	ret
 
-.UpdateRatingBuffers:
-	ld hl, wStringBuffer3
-	ld de, wTempPokedexSeenCount
-	call .UpdateRatingBuffer
-	ld hl, wStringBuffer4
-	ld de, wTempPokedexCaughtCount
-	call .UpdateRatingBuffer
-	ret
-
 .UpdateRatingBuffer:
+	ld a, b
+	ld b, c
+	ld c, a
+	push bc
 	push hl
 	ld a, "@"
 	ld bc, ITEM_NAME_LENGTH
 	call ByteFill
+	ld hl, sp + 2
+	ld d, h
+	ld e, l
 	pop hl
-	lb bc, PRINTNUM_LEFTALIGN | 1, 3
+	lb bc, PRINTNUM_LEFTALIGN | 2, 4
 	call PrintNum
+	pop bc
 	ret
 
 FindOakRating:
 ; return sound effect in de
 ; return text pointer in hl
-	nop
-	ld c, a
-.loop
 	ld a, [hli]
+	ld d, a
+	ld a, [hli]
+	cp b
+	jr c, .next
+	jr nz, .match
+	ld a, d
 	cp c
 	jr nc, .match
-rept 4
-	inc hl
-endr
-	jr .loop
+.next
+	rept 4
+		inc hl
+	endr
+	jr FindOakRating
 
 .match
 	ld a, [hli]
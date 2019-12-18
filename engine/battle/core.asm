3db2cc907df82f05c2d75f1e7e7c0349d6a71dfc
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-12-17 21:07:45 -0300

    Update the Pokédex to use 16-bit IDs
    Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -3545,6 +3545,7 @@ ShowBattleTextEnemySentOut:
 
 ShowSetEnemyMonAndSendOutAnimation:
 	ld a, [wTempEnemyMonSpecies]
+	call SetSeenMon
 	ld [wCurPartySpecies], a
 	ld [wCurSpecies], a
 	call GetBaseData
@@ -6399,11 +6400,7 @@ LoadEnemyMon:
 
 ; Saw this mon
 	ld a, [wTempEnemyMonSpecies]
-	dec a
-	ld c, a
-	ld b, SET_FLAG
-	ld hl, wPokedexSeen
-	predef SmallFarFlagAction
+	call SetSeenMon
 
 	ld hl, wEnemyMonStats
 	ld de, wEnemyStats
